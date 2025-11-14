b3a9f2d9ba6139a644facc2d5ad92aa8c0f93698
Manoj Mourya Neeli <mouryamanoj2505@gmail.com>
2025-11-14 17:13:19 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/triproute/backend/TriprouteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.triproute.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriprouteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriprouteBackendApplication.class, args);
+	}
+
+}
